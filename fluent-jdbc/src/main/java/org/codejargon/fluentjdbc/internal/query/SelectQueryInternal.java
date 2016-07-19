573d5395bf0e3797e577b24771434417ef2c750c
comtel2000 <comtel2000@gmail.com>
2016-07-19 19:20:10 +0200

    Revert "add single parameter api calls and reduce varargs calls"
    This reverts commit bcc59896c4ba1b281ed63310596287e9c72cbb65.

--- fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/internal/query/SelectQueryInternal.java
+++ fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/internal/query/SelectQueryInternal.java
@@ -50,18 +50,6 @@ class SelectQueryInternal extends SingleQueryBase implements SelectQuery {
     }
 
     @Override
-    public SelectQuery param(Object param) {
-        addParameter(param);
-        return this;
-    }
-
-    @Override
-    public SelectQuery params(Object param1, Object param2) {
-        addParameters(param1, param2);
-        return this;
-    }
-    
-    @Override
     public SelectQuery params(List<?> params) {
         addParameters(params);
         return this;
@@ -196,5 +184,4 @@ class SelectQueryInternal extends SingleQueryBase implements SelectQuery {
             );
         }
     }
-
 }
